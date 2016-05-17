1032d31efed00a6787d57a26e829bd52a244e35a
Danny Canaan <danny.canaan@gmail.com>
2016-05-17 16:07:44 +0200

    Allow admin to delete microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
+  before_action :admin_user,     only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -27,6 +28,12 @@ class MicropostsController < ApplicationController
 
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @micropost.nil?
+      redirect_to root_url if @micropost.nil? unless current_user.admin?
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      @micropost = Micropost.all.find_by(id: params[:id])
+      redirect_to(root_url) unless current_user.admin?
     end
 end
